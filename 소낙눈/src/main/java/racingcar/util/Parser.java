4237be6b90ae5405dafbeadc2676f1afba7d7269
snowshower <wonsb25@gmail.com>
2026-09-07 19:38:47 +0900

    refactor: 입력값에 대한 검증 로직 추가

--- 소낙눈/src/main/java/racingcar/util/Parser.java
+++ 소낙눈/src/main/java/racingcar/util/Parser.java
@@ -4,12 +4,68 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
+    private static final String CAR_NAME_BLANK_ERROR_MESSAGE = "[ERROR] 자동차 이름은 공백일 수 없습니다.";
+    private static final String CAR_NAME_EXCEEDING_FIVE_ERROR_MESSAGE = "[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.";
+    private static final String RACING_COUNT_BLANK_ERROR_MESSAGE = "[ERROR] 시도 횟수는 공백일 수 없습니다.";
+    private static final String RACING_COUNT_IS_NOT_NUMBER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
+    private static final String RACING_COUNT_IS_NOT_POSITIVE_ERROR_MESSAGE = "[ERROR] 시도 횟수는 양수여야 합니다.";
 
     public static List<String> parseCarName(String names) {
+        validateNameIsNullOrBlank(names);
+        List<String> carNames = Arrays.asList(names.split(","));
+        validateCarNameIsNullOrBlank(carNames);
+        validateCarNameIsLessThanZeroOrExceedingFive(carNames);
         return Arrays.asList(names.split(","));
     }
 
     public static int parseRacingCount(String count) {
-        return Integer.parseInt(count);
+        validateRacingCountIsNullOrBlank(count);
+        int racingCount = validateRacingCountIsNumber(count);
+        validateRacingCountIsPositive(racingCount);
+        return racingCount;
+    }
+
+    private static void validateNameIsNullOrBlank(String carName) {
+        if (carName == null || carName.isBlank()) {
+            throw new IllegalArgumentException(CAR_NAME_BLANK_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateCarNameIsNullOrBlank(List<String> carNames) {
+        for (String carName : carNames) {
+            validateNameIsNullOrBlank(carName);
+        }
+    }
+
+    private static void validateCarNameIsLessThanZeroOrExceedingFive(List<String> carNames) {
+        for (String carName : carNames) {
+            validateNameExceedingFive(carName);
+        }
+    }
+
+    private static void validateNameExceedingFive(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(CAR_NAME_EXCEEDING_FIVE_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateRacingCountIsNullOrBlank(String racingCount) {
+        if (racingCount == null || racingCount.isBlank()) {
+            throw new IllegalArgumentException(RACING_COUNT_BLANK_ERROR_MESSAGE);
+        }
+    }
+
+    private static int validateRacingCountIsNumber(String racingCount) {
+        try {
+            return Integer.parseInt(racingCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(RACING_COUNT_IS_NOT_NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateRacingCountIsPositive(int racingCount) {
+        if (racingCount < 1) {
+            throw new IllegalArgumentException(RACING_COUNT_IS_NOT_POSITIVE_ERROR_MESSAGE);
+        }
     }
 }
--- 소낙눈/src/test/java/racingcar/util/ParserTest.java
+++ 소낙눈/src/test/java/racingcar/util/ParserTest.java
@@ -1,6 +1,7 @@
 package racingcar.util;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static racingcar.util.Parser.parseCarName;
 import static racingcar.util.Parser.parseRacingCount;
 
@@ -34,5 +35,73 @@ public class ParserTest {
 
         // then
         assertThat(racingCount).isEqualTo(expectCount);
+    }
+
+    @Test
+    void 자동차_이름이_공백이거나_null일_경우_에러가_발생한다() {
+        // given
+        String names1 = "snow, ";
+
+        // when && then
+        assertThatThrownBy(() -> parseCarName(names1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 공백일 수 없습니다.");
+
+        assertThatThrownBy(() -> parseCarName(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 공백일 수 없습니다.");
+    }
+
+    @Test
+    void 자동차_이름이_5자를_초과할_경우_에러가_발생한다() {
+        // given
+        String names1 = "snowshower, snow";
+
+        // when && then
+        assertThatThrownBy(() -> parseCarName(names1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 자동차 이름은 5자를 초과할 수 없습니다.");
+    }
+
+    @Test
+    void 경주_횟수가_공백이거나_null일_경우_에러가_발생한다() {
+        // given
+        String count1 = "";
+        String count2 = null;
+
+        // when && then
+        assertThatThrownBy(() -> parseRacingCount(count1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시도 횟수는 공백일 수 없습니다.");
+        assertThatThrownBy(() -> parseRacingCount(count2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시도 횟수는 공백일 수 없습니다.");
+    }
+
+    @Test
+    void 경주_횟수가_숫자가_아닐_경우_에러가_발생한다() {
+        // given
+        String count = "a";
+
+        // when && then
+        assertThatThrownBy(() -> parseRacingCount(count))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시도 횟수는 숫자여야 합니다.");
+    }
+
+    @Test
+    void 경주_횟수가_양수가_아닐_경우_에러가_발생한다() {
+        // given
+        String count1 = "0";
+        String count2 = "-1";
+
+        // when && then
+        assertThatThrownBy(() -> parseRacingCount(count1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시도 횟수는 양수여야 합니다.");
+
+        assertThatThrownBy(() -> parseRacingCount(count2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 시도 횟수는 양수여야 합니다.");
     }
 }
