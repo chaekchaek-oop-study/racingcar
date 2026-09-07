ace8f3c9cbd892fb82fc0d4c989c645a4c2b9634
snowshower <wonsb25@gmail.com>
2026-09-07 15:44:43 +0900

    feat: 컨트롤러 작성

---
+++ 소낙눈/src/main/java/racingcar/controller/RacingCarController.java
@@ -0,0 +1,46 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.service.ForwardDecisionMaker;
+import racingcar.service.NumberGenerator;
+import racingcar.service.RandomNumberGenerator;
+import racingcar.service.WinnerDecisionMaker;
+import racingcar.util.Parser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingCarController {
+    private final NumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    public void run() {
+        String names = InputView.inputCarNames();
+        List<String> carNames = Parser.parseCarName(names);
+
+        String count = InputView.inputRacingCount();
+        int racingCount = Parser.parseRacingCount(count);
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+
+        OutputView.outputRacingResultMessage();
+
+        for (int i = 0; i < racingCount; i++) {
+            for (Car car : cars) {
+                int randomNumber = randomNumberGenerator.generate();
+                if (ForwardDecisionMaker.isForward(randomNumber)) {
+                    car.setPosition(car.getPosition() + 1);
+                }
+            }
+            OutputView.outputCurrentRacingStatus(cars);
+        }
+
+        List<String> winners = WinnerDecisionMaker.decideWinner(cars);
+
+        OutputView.outputWinner(winners);
+    }
+}
--- 소낙눈/src/main/java/racingcar/domain/Car.java
+++ 소낙눈/src/main/java/racingcar/domain/Car.java
@@ -33,4 +33,8 @@ public class Car {
     public int getPosition() {
         return position;
     }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
