303dfb875d48b33760e884fb8bdcfa31e0d5d37a
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-09-10 17:34:49 +0900

    feat: 프로그램 실행 진입점 구현

--- 아나키/src/main/java/racingcar/Application.java
+++ 아나키/src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.service.RacingCarService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RacingCarService racingCarService = new RacingCarService();
+
+        RacingCarController controller = new RacingCarController(inputView, outputView, racingCarService);
+
+        controller.run();
     }
 }
