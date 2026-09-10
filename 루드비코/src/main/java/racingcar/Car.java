bbcd497823fae376e1ff034ede64ab4b4f32f04c
HyoYoonNam <dev.rudevico@gmail.com>
2026-09-10 17:22:41 +0900

    feat(Car): 자동차의 이름, 위치, 전진 모델링

--- 루드비코/src/main/java/racingcar/Car.java
+++ 루드비코/src/main/java/racingcar/Car.java
@@ -3,12 +3,22 @@ package racingcar;
 public class Car {
 
     private final CarName name;
-    private final Position position;
+    private Position position;
 
     public Car(String name) {
         this.name = new CarName(name);
         this.position = Position.START;
     }
 
-    // 추가 기능 구현
+    public void moveForward() {
+        position = position.increase();
+    }
+
+    public int currentPosition() {
+        return position.value();
+    }
+
+    public String name() {
+        return name.value();
+    }
 }
---
+++ 루드비코/src/test/java/racingcar/CarTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    void 이름을_가진다() {
+        Car car = new Car("jason");
+
+        assertThat(car.name()).isEqualTo("jason");
+    }
+
+    @Test
+    void 초기_위치는_0이다() {
+        Car car = createCarWithDefaultName();
+
+        assertThat(car.currentPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 전진하면_위치가_1_증가한다() {
+        Car car = createCarWithDefaultName();
+        int beforeMoving = car.currentPosition();
+
+        car.moveForward();
+
+        assertThat(car.currentPosition()).isEqualTo(beforeMoving + 1);
+    }
+
+    private static Car createCarWithDefaultName() {
+        return new Car("pobi");
+    }
+}
