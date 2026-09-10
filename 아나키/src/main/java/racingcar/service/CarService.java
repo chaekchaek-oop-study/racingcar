aa053ea658960aa6bfbd291a2c07fe502df40f25
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-09-10 17:30:13 +0900

    feat: 한 라운드 게임 진행 기능 구현

--- 아나키/src/main/java/racingcar/service/CarService.java
+++ 아나키/src/main/java/racingcar/service/CarService.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import racingcar.domain.Car;
@@ -11,5 +12,14 @@ public class CarService {
             cars.add(new Car(carName));
         }
         return cars;
+    }
+
+    public void playRound(List<Car> cars) {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            if (4 <= randomNumber) {
+                car.move();
+            }
+        }
     }
 }
