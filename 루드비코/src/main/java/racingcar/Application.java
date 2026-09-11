0a39250be395d4076c13ba446198fba7c4a2deb4
HyoYoonNam <dev.rudevico@gmail.com>
2026-09-11 13:31:40 +0900

    feat: 입출력을 Application에 구현

--- 루드비코/src/main/java/racingcar/Application.java
+++ 루드비코/src/main/java/racingcar/Application.java
@@ -1,7 +1,33 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import racingcar.car.Car;
+import racingcar.move.MovingStrategy;
+import racingcar.move.RandomNumberGenerator;
+import racingcar.race.GameResult;
+import racingcar.race.RacingGame;
+import racingcar.race.RoundCount;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String names = Console.readLine();
+        String round = Console.readLine();
+        RacingGame racingGame = new RacingGame(Arrays.stream(names.split(",")).map(Car::new).toList(),
+                new MovingStrategy(new RandomNumberGenerator(0, 9)));
+        GameResult gameResult = racingGame.play(new RoundCount(Integer.parseInt(round)));
+
+        System.out.println("실행 결과");
+        gameResult.rounds()
+                .forEach(roundResult -> {
+                    roundResult.snapshots()
+                            .forEach(snapshot -> {
+                                System.out.println(snapshot.name() + " : " + "-".repeat(snapshot.position()));
+                            });
+                    System.out.println();
+                });
+        System.out.println("최종 우승자 : " + gameResult.winners().stream().collect(Collectors.joining(", ")));
     }
 }
--- 루드비코/src/test/java/racingcar/ApplicationTest.java
+++ 루드비코/src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,7 +8,6 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Disabled
 class ApplicationTest extends NsTest {
 
     private static final int MOVING_FORWARD = 4;
