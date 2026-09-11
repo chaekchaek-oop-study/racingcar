e31f9d9aced1c22b575481954fbdc269770037be
HyoYoonNam <dev.rudevico@gmail.com>
2026-09-11 16:14:51 +0900

    refactopr(OutputView): 콘솔 출력 책임을 Application에서 OutputView로 위임

--- 루드비코/src/main/java/racingcar/Application.java
+++ 루드비코/src/main/java/racingcar/Application.java
@@ -1,7 +1,6 @@
 package racingcar;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import racingcar.car.Car;
 import racingcar.move.MovingStrategy;
 import racingcar.move.RandomNumberGenerator;
@@ -10,23 +9,21 @@ import racingcar.race.RacingGame;
 import racingcar.race.RoundCount;
 
 public class Application {
+
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
         List<String> carNames = inputView.readCarNames();
         int roundCount = inputView.readRoundCount();
+
         RacingGame racingGame = new RacingGame(carNames.stream().map(Car::new).toList(),
                 new MovingStrategy(new RandomNumberGenerator(0, 9)));
         GameResult gameResult = racingGame.play(new RoundCount(roundCount));
 
-        System.out.println("실행 결과");
+        outputView.printResultHeader();
         gameResult.rounds()
-                .forEach(roundResult -> {
-                    roundResult.snapshots()
-                            .forEach(snapshot -> {
-                                System.out.println(snapshot.name() + " : " + "-".repeat(snapshot.position()));
-                            });
-                    System.out.println();
-                });
-        System.out.println("최종 우승자 : " + gameResult.winners().stream().collect(Collectors.joining(", ")));
+                .forEach(outputView::printRoundResult);
+        outputView.printWinners(gameResult.winners());
     }
 }
