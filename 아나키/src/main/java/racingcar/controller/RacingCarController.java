2ae7b036aaab80436d6291a6433fc053421f587e
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-09-11 13:24:07 +0900

    refactor: 기능 단위 메서드 분리

--- 아나키/src/main/java/racingcar/controller/RacingCarController.java
+++ 아나키/src/main/java/racingcar/controller/RacingCarController.java
@@ -21,29 +21,12 @@ public class RacingCarController {
 
     public void run() {
         String inputCarNames = inputView.inputCarNames();
-        List<String> carNames =  Arrays.stream(inputCarNames.split(","))
-                .map(String::trim)
-                .toList();
-        for (String carName : carNames) {
-            if (carName.isBlank()){
-                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
-            }
-            if (carName.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
-            }
-        }
-        List<Car> cars = carService.createCars(carNames);
+        List<String> carNames = getCarName(inputCarNames);
 
         String inputTryCount = inputView.inputTryCount();
-        if (inputTryCount.isBlank()) {
-            throw new IllegalArgumentException("시도 횟수는 공백일 수 없습니다.");
-        }
-        int tryCount;
-        try {
-            tryCount = Integer.parseInt(inputTryCount);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 숫자만 입력할 수 있습니다.");
-        }
+        int tryCount = getTryCount(inputTryCount);
+
+        List<Car> cars = carService.createCars(carNames);
 
         outputView.printResult();
 
@@ -54,5 +37,39 @@ public class RacingCarController {
 
         List<String> winnerName = carService.getWinner(cars);
         outputView.printWinner(winnerName);
+    }
+
+    private List<String> getCarName(String inputCarNames) {
+        List<String> carNames =  Arrays.stream(inputCarNames.split(","))
+                .map(String::trim)
+                .toList();
+
+        for (String carName : carNames) {
+            if (carName.isBlank()){
+                throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다.");
+            }
+
+            if (carName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+            }
+        }
+
+        return carNames;
+    }
+
+    private int getTryCount(String inputTryCount) {
+        int tryCount;
+
+        if (inputTryCount.isBlank()) {
+            throw new IllegalArgumentException("시도 횟수는 공백일 수 없습니다.");
+        }
+
+        try {
+            tryCount = Integer.parseInt(inputTryCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 숫자만 입력할 수 있습니다.");
+        }
+
+        return tryCount;
     }
 }
